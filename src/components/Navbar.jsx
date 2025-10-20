8c421b0510e4edb957f8c9e5ad6e0cb84071d38f
MrHryhorii <hryhorii.kodehode@gmail.com>
2025-10-20 14:52:29 +0200

    navbar stop

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,13 @@ const Navbar = () => {
   return (
     <nav>
         <div>
-            
+            <p>LOGO</p>
         </div>
         <div>
-
+            <p>About</p>
+            <p>Projects</p>
+            <p>Contacts</p>
+            <p>Resume</p>
         </div>
     </nav>
   )
