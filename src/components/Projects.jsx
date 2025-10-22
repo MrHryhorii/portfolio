dbb51265571e9179590d8b013e8edd6558afa728
MrHryhorii <hryhorii.kodehode@gmail.com>
2025-10-22 13:58:37 +0200

    extra projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,7 @@ import BigSlide from './BigSlide'
 
 const Projects = () => {
   return (
+    <>
     <section className='projects'>
       <h1>Projects</h1>
       <div className='projects-flex'>
@@ -19,12 +20,61 @@ const Projects = () => {
                 their medication historyfrom the past or look to the future and see what they need to take.
                 Finally, users are also given a map view so that they have no problem finding any nearby pharmacies
                 or hostiptals if needed.</p>
+              <p>`</p>
+              <p>Python · Discord API · Johns Hopkins API</p>
+              <p>Source Code</p>
             </article>
           </BigSlide>
         </div>
       </div> 
 
     </section>
+
+    <section className='project-2'>
+      <div className='projects-flex'>
+        <div className='projects-text'>
+          <BigSlide dir="left" distance={150} duration={2000} cascade damping={0.14} triggerOnce={false} fraction={0}>
+            <article>
+              <h2>Covid-19 Discord Bot</h2>
+              <p>A hackathon project for HackED 2021. Using a variety of commands you can retrieve different
+                Covid-19 data including cases, test, and vaccines.</p>
+              <p>`</p>
+              <p>Python · Discord API · Johns Hopkins API</p>
+              <p>Source Code</p>
+            </article>
+          </BigSlide>
+        </div>
+        <div className='projects-img'>
+          <div className='aabb-2'></div>
+        </div>
+      </div> 
+
+    </section>
+
+    <section className='project-3'>
+      <div className='projects-flex'>
+        <div className='projects-img'>
+          <div className='aabb-2'></div>
+        </div>
+        <div className='projects-text'>
+          <BigSlide dir="right" distance={150} duration={2000} cascade damping={0.14} triggerOnce={false} fraction={0}>
+            <article>
+              <h2>Flavour Flow</h2>
+              <p>A hackathon project for Hack The North 2021.
+                Allows users to enter ingredients and get recipes.
+                The recipe generator maximizes given ingredients and minimizes any missing ingredients,
+                using the Spoonacular API.
+              </p>
+              <p>`</p>
+              <p>Python · Discord API · Johns Hopkins API</p>
+              <p>Source Code</p>
+            </article>
+          </BigSlide>
+        </div>
+      </div> 
+
+    </section>
+    </>
   )
 }
 
