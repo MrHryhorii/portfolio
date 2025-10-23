6838655047ed1795b3aea58e204972428b6d0174
MrHryhorii <hryhorii.kodehode@gmail.com>
2025-10-23 13:07:38 +0200

    footer css module

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
+import styles from '../css/Footer.module.css';
 
 const Footer = () => {
   return (
-    <div className='footer'>
-        <div className='footer-row'>
-            <div className='footer-left'>
-                <div className='footer-line'></div>
+    <div className={`${styles.footer}`}>
+        <div className={`${styles.footerRow}`}>
+            <div className={`${styles.footerLeft}`}>
+                <div className={`${styles.footerLine}`}></div>
             </div>
-            <div className='footer-center'>
-                <div className='hero-icon-1'></div>
-                <div className='hero-icon-1'></div>
+            <div className={`${styles.footerCenter}`}>
+                <div className={`${styles.heroIcon1}`}></div>
+                <div className={`${styles.heroIcon2}`}></div>
             </div>
-            <div className='footer-right'>
-                <div className='footer-line'></div>
+            <div className={`${styles.footerRight}`}>
+                <div className={`${styles.footerLine}`}></div>
             </div>
         </div>
     </div>
