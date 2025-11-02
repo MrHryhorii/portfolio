f6954323b910e184180d09124fac290280213066
MrHryhorii <hryhorii.kodehode@gmail.com>
2025-11-02 23:30:29 +0100

    nav logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import styles from '../css/Navbar.module.css';
 
+import logo from '../assets/tsubaki_lite.svg'
+
 const Navbar = ({scrollToId}) => {
 
   return (
     <nav className={`${styles.nav}`}>
         <div className={`${styles.navDiv}`}>
-            <a href="#" onClick={(e) => { e.preventDefault(); scrollToId('hero'); }}><p>Logo</p></a>
+            <a href="#" onClick={(e) => { e.preventDefault(); scrollToId('hero'); }}><img className={`${styles.logoImg}`} src={logo} alt="me" /></a>
         </div>
         <div className={`${styles.navDiv} ${styles.navDivLast}`}>
             <a href="#" onClick={(e) => { e.preventDefault(); scrollToId('about'); }}><p>About</p></a>
